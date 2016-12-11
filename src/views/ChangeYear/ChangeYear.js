7364440dc7289acb9350310ceddb8f7f4705fc4d
James Clements <jamesvclements@dyn098224.cc.lehigh.edu>
2016-12-11 15:39:45 -0500

    resolves #41

--- src/views/ChangeYear/ChangeYear.js
+++ src/views/ChangeYear/ChangeYear.js
@@ -1,5 +1,5 @@
 import React, { PropTypes as T } from 'react'
-import { Grid, Row, Col, Breadcrumb, Panel, PageHeader } from 'react-bootstrap'
+import { Grid, Row, Col, Breadcrumb, Panel, PageHeader, Modal, Button } from 'react-bootstrap'
 import './ChangeYear.css'
 
 export class ChangeYear extends React.Component {
@@ -13,125 +13,131 @@ export class ChangeYear extends React.Component {
 		super(props)
 
 		this.state = {
-			year: 0
+			year: 0,
+			showWarningModal: false
 		}
 
 		fetch(`${process.env.REACT_APP_SERVER_ADDRESS}/api/change-year`,
-		{
-			method: 'GET',
-		})
-		.then(response => {
+			{
+				method: 'GET',
+			})
+			.then(response => {
 				response.json().then(year => {
-					if(response.ok){
-						console.log(year)
+					if (response.ok) {
 						this.setState({
 							year: year
 						})
+					} else {
+						this.context.addNotification({
+							title: 'Error',
+							message: 'Failed to get year',
+							level: 'error'
+						})
 					}
 				})
-			
-		})
-		.catch(err => {
-			console.error(err)
-			this.context.addNotification({
-				title: 'Error',
-				message: 'Failed to get year',
-				level: 'error'
-			})
-		})
 
+			})
+			.catch(err => {
+				console.error(err)
+				this.context.addNotification({
+					title: 'Error',
+					message: 'Failed to get year',
+					level: 'error'
+				})
+			})
 	}
 
-	incrementYear(){
+	incrementYear() {
 		fetch(`${process.env.REACT_APP_SERVER_ADDRESS}/api/change-year`,
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				increment: 'Y'
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					increment: 'Y'
+				})
 			})
-		})
-		.then(() => {
-			this.context.addNotification({
-				title: 'Success',
-				message: `Successfully advanced to the next year`,
-				level: 'success'
+			.then(response => {
+				if (response.ok) {
+					this.context.addNotification({
+						title: 'Success',
+						message: `Successfully advanced to the next year`,
+						level: 'success'
+					})
+					this.setState({
+						year: this.state.year + 1
+					})
+					this.hideWarning()
+				} else {
+					this.context.addNotification({
+						title: 'Error',
+						message: 'Failed to advance year',
+						level: 'error'
+					})
+				}
 			})
-			this.setState({
-				year: this.state.year+1
+			.catch(err => {
+				console.error(err)
+				this.context.addNotification({
+					title: 'Error',
+					message: 'Failed to advance year',
+					level: 'error'
+				})
 			})
-		})
-		.catch(err => {
-			console.error(err)
-			this.context.addNotification({
-				title: 'Error',
-				message: 'Failed to advance year',
-				level: 'error'
-			})
+	}
+
+	showWarning() {
+		this.setState({
+			showWarningModal: true
 		})
 	}
 
-	decrementYear(){
-		fetch(`${process.env.REACT_APP_SERVER_ADDRESS}/api/change-year`,
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				increment: 'N'
-			})
-		})
-		.then(() => {
-			this.context.addNotification({
-				title: 'Success',
-				message: `Successfully went back to the previous year`,
-				level: 'success'
-			})
-			this.setState({
-				year: this.state.year-1
-			})
-		})
-		.catch(err => {
-			console.error(err)
-			this.context.addNotification({
-				title: 'Error',
-				message: 'Failed to go back year',
-				level: 'error'
-			})
+	hideWarning() {
+		this.setState({
+			showWarningModal: false
 		})
 	}
 
 	render() {
 		return (
 			<div className="root">
-			<Breadcrumb>
-			<Breadcrumb.Item href="#/landing">
-			Home
+				<Breadcrumb>
+					<Breadcrumb.Item href="#/landing">
+						Home
 			</Breadcrumb.Item>
-			<Breadcrumb.Item href="#/admin">
-			Admin
+					<Breadcrumb.Item href="#/admin">
+						Admin
 			</Breadcrumb.Item>
-			<Breadcrumb.Item active>
-			Change-Year
+					<Breadcrumb.Item active>
+						Change Year
 			</Breadcrumb.Item>
-			</Breadcrumb>
-			<PageHeader>{`Current Academic Year: ${this.state.year} - ${this.state.year + 1}`}</PageHeader>
-			<Grid>
-			<Row>
-			<Col xs={12}>
-			<Panel
-			onClick={this.incrementYear.bind(this)}
-			className="admin-panel">
-			<h3>Go to Next Year</h3>
-			</Panel>
-			</Col>
-			</Row>
-			</Grid>
+				</Breadcrumb>
+				<PageHeader>{`Current Academic Year: ${this.state.year} - ${this.state.year + 1}`}</PageHeader>
+				<Grid>
+					<Row>
+						<Col xs={12}>
+							<Panel
+								onClick={this.showWarning.bind(this)}
+								className="clickable-panel">
+								<h3>Advance Year</h3>
+							</Panel>
+						</Col>
+					</Row>
+				</Grid>
+				<Modal show={this.state.showWarningModal} onHide={this.hideWarning.bind(this)}>
+					<Modal.Body>
+						<h4>WARNING</h4>
+						<p>This is an irreversible action.</p>
+						<p>Upon advancing the year, the student data will be altered to the new year.</p>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button bsStyle="danger" onClick={this.incrementYear.bind(this)}>ADVANCE YEAR</Button>
+						<Button onClick={this.hideWarning.bind(this)}>CLOSE</Button>
+					</Modal.Footer>
+				</Modal>
 			</div>
-			)
+		)
 	}
 }
 
